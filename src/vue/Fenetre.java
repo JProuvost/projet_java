3853ce10b1c7009f1e5ac41dcfd8fd9da45773cb
JProuvost <35262419+JProuvost@users.noreply.github.com>
2019-06-08 17:48:16 +0200

    adding start of view

---
+++ src/vue/Fenetre.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vue;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Fenetre extends JFrame{
+
+  private static CardLayout cl = new CardLayout();
+  public static JPanel content = new JPanel();
+  Page accueil = new Page();
+  Selannee selannee = new Selannee();
+  //Liste des noms de nos conteneurs pour la pile de cartes
+
+  public Fenetre(){
+    this.setTitle("Gestion d'école");
+    this.setSize(300, 120);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLocationRelativeTo(null);
+		
+    //On crée trois conteneurs de couleur différente
+    JPanel card1 = new JPanel();		
+    JPanel card2 = new JPanel();		
+    
+    JPanel boutonPane = new JPanel();
+    JButton bouton = new JButton("Contenu suivant");
+    //Définition de l'action du bouton
+    bouton.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent event){
+        //Via cette instruction, on passe au prochain conteneur de la pile
+        cl.next(content);
+      }
+    });
+    
+    boutonPane.add(bouton);
+    //On définit le layout
+    content.setLayout(cl);
+    //On ajoute les cartes à la pile avec un nom pour les retrouver
+    content.add(accueil.get_panel(), "accueil");
+    content.add(selannee, "selannee");
+    
+    this.getContentPane().add(boutonPane, BorderLayout.NORTH);
+    this.getContentPane().add(content, BorderLayout.CENTER);
+    this.setVisible(true);
+  }
+  public static void changePanel(String a)
+    {
+        cl.show(content,a);
+    }
+}
