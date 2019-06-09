ece56d40d650783c74ad6321c6d2275fef75d626
Mikhali <33858877+Mikhali@users.noreply.github.com>
2019-06-09 18:37:19 +0200

    Add files via upload

--- src/Chart/LineChart_AWT.java
+++ src/Chart/LineChart_AWT.java
@@ -5,15 +5,14 @@
  */
 package Chart;
 import java.util.Map;
+import javax.swing.JFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
-public class LineChart_AWT extends ApplicationFrame {
+public class LineChart_AWT extends JFrame {
 
    public LineChart_AWT( String applicationTitle , String chartTitle , Map<Integer,Integer> data) {
       super(applicationTitle);
@@ -29,16 +28,15 @@ public class LineChart_AWT extends ApplicationFrame {
       setContentPane( chartPanel );
    }
 
-   private DefaultCategoryDataset createDataset( Map<Integer,Integer> data) {
-      DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
+    private DefaultCategoryDataset createDataset( Map<Integer,Integer> data) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
       
-      for(Map.Entry<Integer, Integer> entry : data.entrySet()){
-        System.out.println(entry.getKey());
+    for(Map.Entry<Integer, Integer> entry : data.entrySet()){
         dataset.addValue(entry.getValue() , "schools" , entry.getKey());
-      }
-      
-      return dataset;
-   }
+    }
+
+        return dataset;
+    }
    
    
 }
